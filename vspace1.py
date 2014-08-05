36e7048e04fdc7d95bd5aaff50434188498f82d2
autogit <autogit@zilka.me>
2014-08-05 17:59:28 +0200

    automatic

--- vspace1.py
+++ vspace1.py
@@ -30,7 +30,7 @@ class VSpace1:
     dialog_cnt = 10
     lat_dims = 2
     proj_dims = 3
-    learning_iters = 100
+    learning_iters = 300
     learning_rate = 0.1
     rprop_plus = 1.2
     rprop_minus = 0.5
