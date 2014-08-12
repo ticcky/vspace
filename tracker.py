8522d8958b6047be356a8685f02b95450b84c603
Lukas Zilka <lukas@zilka.me>
2014-08-12 18:27:16 +0200

    Automatic.

--- tracker.py
+++ tracker.py
@@ -41,9 +41,8 @@ class Tracker:
 
     def new_dialog(self):
         self.state = np.zeros(self.model.lat_dims)
-        last_act = None
         for act in self.model.nulls:
-            self.state = self.model.f_s_new(self.state, act)
+            self.state = self.model.f_s_new(self.state, self.model.acts[act])
 
         self.true_state = {slot: self.model.ontology[slot][0] for slot in self.model.slots}
 
