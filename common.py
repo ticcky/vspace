d67fc62a1cc267ea4a5f0bad20e7446cce77bbe1
Lukas Zilka <lukas@zilka.me>
2014-08-12 16:45:29 +0200

    Automatic.

--- common.py
+++ common.py
@@ -4,4 +4,5 @@ import theano
 
 def rand(*args):
     #return np.zeros(args).astype(theano.config.floatX)
+    return np.random.rand(*args).astype(theano.config.floatX)
     return np.random.randn(*args).astype(theano.config.floatX)
