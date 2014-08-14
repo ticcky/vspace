47d9e08a48b02efeb56f8407bc3cb820dac8f753
Lukas Zilka <lukas@zilka.me>
2014-08-14 15:52:46 +0200

    Automatic.

--- vspace1.py
+++ vspace1.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 import copy
 import itertools
+import multiprocessing
 import os
 import pickle
 import pprint
@@ -238,6 +239,38 @@ class VSpace1:
         if debug:
             print "> Starting learning iter"
 
+        pool = multiprocessing.Pool(self.n_processes)
+        def compute_gradient((model, dialog, )):
+            tracker = Tracker(model)
+            tracker.new_dialog()
+            last_state = tracker.get_state()
+
+            accum_loss_grad = []
+            for shape in self.model.shapes:
+                accum_loss_grad.append(np.zeros(shape, dtype=theano.config.floatX))
+
+            for act in dialog:
+                act_ndx = model.acts[act]
+
+                # Run tracker to get the new state and the true state.
+                curr_state, true_state = tracker.next(act)
+
+                # Compute the loss & gradient of the loss.
+                val = [model.values[true_state[slot]] for slot in model.slots]
+
+                total_loss += model.f_curr_slot_loss(curr_state, val)
+
+                for param_loss_grad, accum in zip(model.loss_grads, accum_loss_grad):
+                    accum += 1.0 / n_data * param_loss_grad(last_state, act_ndx, val)
+
+                last_state = curr_state
+
+            return accum_loss_grad
+
+        res = pool.map(compute_gradient, self.training_dialogs)
+
+        import ipdb; ipdb.set_trace()
+
         # Prepare accumulators for gradient.
         accum_loss_grad = []
         for shape in self.model.shapes:
@@ -251,7 +284,7 @@ class VSpace1:
         for dialog in self.training_dialogs:
             if debug:
                 print ">> New dialog"
-
+            tracker = Tracker(self.model)
             tracker.new_dialog()
             last_state = tracker.get_state()
             for act in dialog:
