8aab5ad9b7e82ac8b8f9d94f5b83e206e128d11b
Lukas Zilka <lukas@zilka.me>
2014-08-14 16:01:23 +0200

    Automatic.

--- vspace1.py
+++ vspace1.py
@@ -240,34 +240,8 @@ class VSpace1:
         if debug:
             print "> Starting learning iter"
 
+        from vspace1_computer import compute_gradient
         pool = multiprocessing.Pool(self.n_processes)
-        def compute_gradient((model, dialog, )):
-            tracker = Tracker(model)
-            tracker.new_dialog()
-            last_state = tracker.get_state()
-
-            accum_loss_grad = []
-            for shape in self.model.shapes:
-                accum_loss_grad.append(np.zeros(shape, dtype=theano.config.floatX))
-
-            for act in dialog:
-                act_ndx = model.acts[act]
-
-                # Run tracker to get the new state and the true state.
-                curr_state, true_state = tracker.next(act)
-
-                # Compute the loss & gradient of the loss.
-                val = [model.values[true_state[slot]] for slot in model.slots]
-
-                total_loss += model.f_curr_slot_loss(curr_state, val)
-
-                for param_loss_grad, accum in zip(model.loss_grads, accum_loss_grad):
-                    accum += 1.0 / n_data * param_loss_grad(last_state, act_ndx, val)
-
-                last_state = curr_state
-
-            return accum_loss_grad
-
         res = pool.map(compute_gradient, self.training_dialogs)
 
         import ipdb; ipdb.set_trace()
