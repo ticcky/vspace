1cd4fa8c7762778f851b8350cd4384970a4c2755
Lukas Zilka <lukas@zilka.me>
2014-08-21 16:16:15 +0200

    Automatic.

--- vspace2.py
+++ vspace2.py
@@ -214,7 +214,8 @@ class VSpace1:
                 #loss += ((proj[slot_ndx] - b[data[slot_ndx]])**2).sum()
                 #continue
 
-                for val in [0] + random.sample(self.gen.ontology[slot][1:], 4):
+                for val in [self.gen.ontology[slot][0]] + random.sample(
+                        self.gen.ontology[slot][1:], 4):
                     val_ndx = self.model.values[val]
                     score = ((proj[slot_ndx] - b[val_ndx])**2).sum()
                     loss += T.eq(data[slot_ndx], val_ndx) * score
