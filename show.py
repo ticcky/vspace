cf443df0ae2e990af3854f6500fe7e0c31897952
Lukas Zilka <lukas@zilka.me>
2014-10-16 16:33:06 +0200

    show

---
+++ show.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# encoding: utf8
+
+import json
+import os
+import sys
+
+
+def show_experiment(experiment_name, path):
+    with open(path) as f_in:
+        data = json.loads(f_in.read())
+    print data['mean_score']
+    #print experiment_name, data['mean_score']
+
+
+def main():
+    base_dir = sys.argv[1]
+    for exp in os.listdir(base_dir):
+        dir_name = os.path.join(base_dir, exp)
+        exp_val = exp
+        print ">", dir_name
+
+        for cnt, exp_res in sorted((int(x[:x.index('.')]), x) for x in os.listdir(dir_name)):
+            exp_res_val = exp_res
+            if len(exp_res.rstrip('.json')) == 2:
+                exp_res_val = '0' + exp_res.rstrip('.json')
+
+            if exp_res.endswith('.json'):
+                show_experiment(
+                    exp_val + "-" + exp_res_val,
+                    os.path.join(dir_name, exp_res)
+                )
+
+
+if __name__ == '__main__':
+    main()
